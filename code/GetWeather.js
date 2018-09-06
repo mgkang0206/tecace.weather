160c8034b0adcbb187473ac5a4b5c19e741662f6
Michael Kang <michaelkang@tecace.com>
2018-09-06 12:28:00 -0700

    image

--- code/GetWeather.js
+++ code/GetWeather.js
@@ -1,21 +1,29 @@
 module.exports.function = function getWeather (city) {
 
-  var url = "https://api.openweathermap.org"
-  var picUrl = "https://api.unsplash.com"
+  //Url for Weather Api Call.
+  const url = "https://api.openweathermap.org";
+  const weatherQuery ="/data/2.5/weather?units=imperial&APPID=";
+  
+  //Url for Unsplash Api Call for Card Image.
+  const picUrl = "https://api.unsplash.com";
+  const picQuery ="/search/photos?client_id="
+  
+  //Sets the format of the Api callback
   var options = {
     format: 'json',
-  }
+  };
   
-  var res = http.getUrl(url + "/data/2.5/weather?units=imperial&APPID=8267725a41bb55c1299b9d5128e04afd&q="+city,options)
-  var forecasts = res.weather[0].description
+  //Making an Api Call to the Weather Api
+  var res = http.getUrl(url + weatherQuery + "8267725a41bb55c1299b9d5128e04afd" + "&q=" + city,options);
+  var forecasts = res.weather[0].description;
   
-  
-  var randomNum = Math.floor((Math.random()*4))
-  var pic = http.getUrl(picUrl + "/search/photos?client_id=c7f97f426a9e86beea12b9f5ff796ae7b0d02074fc0fc0df45919e0cb57f9e28&query="+city+"%20landmark&page=1",options)
-  var picSrc = pic.results[randomNum].urls.small
+  //Making an Api Call to the Image Api
+  var randomNum = Math.floor((Math.random()*4));
+  var pic = http.getUrl(picUrl + picQuery + "c7f97f426a9e86beea12b9f5ff796ae7b0d02074fc0fc0df45919e0cb57f9e28" + "&query=" +city+ "%20landmark&page=1",options);
+  var picSrc = pic.results[randomNum].urls.small;
   
 
-  
+  //Returning the necessary properties of the Weather.model(The Goal)
   return  { city: city.charAt(0).toUpperCase()+city.slice(1), 
             forecast: forecasts, 
             temperature: res.main.temp, 
