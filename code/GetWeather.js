be0ef2e6f90754e797d0605f03c602a749428d40
Michael Kang <michaelkang@tecace.com>
2018-09-06 18:04:19 -0700

    API KEY

--- code/GetWeather.js
+++ code/GetWeather.js
@@ -14,12 +14,12 @@ module.exports.function = function getWeather (city) {
   };
   
   //Making an Api Call to the Weather Api
-  var res = http.getUrl(url + weatherQuery + "8267725a41bb55c1299b9d5128e04afd" + "&q=" + city,options);
+  var res = http.getUrl(url + weatherQuery + "*YOUR API KEY HERE*" + "&q=" + city,options);
   var forecasts = res.weather[0].description;
   
   //Making an Api Call to the Image Api
   var randomNum = Math.floor((Math.random()*4));
-  var pic = http.getUrl(picUrl + picQuery + "c7f97f426a9e86beea12b9f5ff796ae7b0d02074fc0fc0df45919e0cb57f9e28" + "&query=" +city+ "%20landmark&page=1",options);
+  var pic = http.getUrl(picUrl + picQuery + "*YOUR API KEY HERE*" + "&query=" +city+ "%20landmark&page=1",options);
   var picSrc = pic.results[randomNum].urls.small;
   
 
